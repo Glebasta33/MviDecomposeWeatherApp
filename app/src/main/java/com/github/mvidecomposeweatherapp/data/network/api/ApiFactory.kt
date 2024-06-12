7237718bbe701213d48f63971f9f54904a06855d
Gleb Trusov <gleb.trusow@yandex.ru>
2024-06-12 10:05:35 +0300

    Добавил локализацию ответа от API

--- app/src/main/java/com/github/mvidecomposeweatherapp/data/network/api/ApiFactory.kt
+++ app/src/main/java/com/github/mvidecomposeweatherapp/data/network/api/ApiFactory.kt
@@ -5,11 +5,13 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
+import java.util.Locale
 
 object ApiFactory {
 
     private const val BASE_URL = "https://api.weatherapi.com/v1/"
     private const val KEY_PARAM = "key"
+    private const val KEY_LANG = "lang"
 
     private val okHttpClient = OkHttpClient.Builder()
         .addInterceptor { chain ->
@@ -17,6 +19,8 @@ object ApiFactory {
 
             val newUrl = originalRequest.url().newBuilder()
                 .addQueryParameter(KEY_PARAM, BuildConfig.WEATHER_API_KEY)
+                .addQueryParameter(KEY_LANG, Locale.getDefault().language)
+
                 .build()
 
             val newRequest = originalRequest.newBuilder()
